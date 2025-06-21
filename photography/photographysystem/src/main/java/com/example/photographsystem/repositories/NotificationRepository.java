3b2aea1ebf283a2fc594f4d16dd1e3fa7949b35f
Tharushika W.A.M <it22914200@my.sliit.lk>
2025-06-21 23:54:23 +0530

    Create NotificationRepository.java
    add notification repository

---
+++ photography/photographysystem/src/main/java/com/example/photographsystem/repositories/NotificationRepository.java
@@ -0,0 +1,16 @@
+package com.example.photographsystem.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.photographsystem.models.Notification;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByReceiverIdAndDeleteStatusFalse(String receiverId);
+    List<Notification> findAllByDeleteStatusFalse();
+    Optional<Notification> findByIdAndDeleteStatusFalse(String id);
+}
